55e829e80c3eb065fc7864711c5ec0b6bf504267
Neelabhro Roy <neelabhro16171@iiitd.ac.in>
2019-02-23 03:40:50 +0530

    CMC Included

--- reid.m
+++ reid.m
@@ -10,8 +10,8 @@ close all;
 %figure;
 %x2 = imread('VIPeR/cam_b/000_45.bmp');  
 %imshow(x2);
-
-numClass = 316;
+%% load the extracted LOMO features
+numClass = 632;
 numFolds = 10;
 numRanks = 100;
 
@@ -19,13 +19,15 @@ numRanks = 100;
 load('/viper_lomo.mat', 'descriptors');
 %Gallery Features
 %galFea = descriptors(1 : numClass, 1:100);
-galFea = descriptors(1 : numClass,:);
+%galFea = descriptors(1 : numClass,:);
+galFea = descriptors(1 : numClass, :);
+probeFea = descriptors(numClass + 1 : end, :);
 galFea = transpose(galFea);
 %Probe Features
-probeFea = descriptors(numClass*2 + 1 : numClass*3, :);
+%probeFea = descriptors(numClass*2 + 1 : numClass*3, :);
 probeFea = transpose(probeFea);
 %probFea = descriptors(numClass + 1 : end, 1:100);
-clear descriptors
+%clear descriptors
 images = zeros(128,48,3,316,'uint8');
 camA = dir(['VIPeR/cam_a/*.bmp']);
 camB = dir(['VIPeR/cam_b/*.bmp']);
@@ -55,8 +57,8 @@ Lp = 0.2;
 nu = 1;
 beta = 1;
 
-n = 316;
-d = 316;
+n = 632;
+d = 632;
 k = d;
 
 %probeFeaT = transpose(probeFea);
@@ -103,7 +105,7 @@ P2 = randi([0, 1], [k,d]);
 
 %V1T = V1';
 
-%Main algorithm
+%% Main algorithm
 U  = (( X1 * transpose(V1)) + ( X2 * transpose(V2))) .* inv((( V1 * transpose(V1)) + ( V2 * transpose(V2)) + (Lu*eye(k))));
 V1 = inv(((transpose(U) * U) + (nu + beta + Lv) * eye(k))) * ((transpose(U) * X1) + (beta* A * V2) + nu * P1 * X1);
 V2 = inv(((transpose(U) * U) + ( beta * transpose(A) * A) + (nu + Lv) .* eye(k))) * ((transpose(U) * X2) + (beta* transpose(A) * V1) + nu * P2 * X2);
@@ -115,6 +117,63 @@ D = zeros(n);
 for m = 1:n
     v1 = P1*X1(:,m);
     v2 = P2*X2(:,m);
-    D(m) = sqrt(sum((v1 - A*v2).^2));
+    D(m) = norm(((v1 - A*v2).^2));
 end
-D = D(:,1);
+%D = D(:,1);
+
+clear descriptors
+
+%% set the seed of the random stream. The results reported in our CVPR 2015 paper are achieved by setting seed = 0. 
+seed = 0;
+rng(seed);
+
+%% evaluate
+cms = zeros(numFolds, numRanks);
+
+for nf = 1 : numFolds
+    p = randperm(numClass);
+    
+    galFea1 = galFea( p(1:numClass/2), : );
+    probFea1 = probeFea( p(1:numClass/2), : );
+    
+    t0 = tic;
+%    [W, M] = XQDA(galFea1, probFea1, (1:numClass/2)', (1:numClass/2)');
+
+    %{
+    %% if you need to set different parameters other than the defaults, set them accordingly
+    options.lambda = 0.001;
+    options.qdaDims = -1;
+    options.verbose = true;
+    [W, M] = XQDA(galFea1, probFea1, (1:numClass/2)', (1:numClass/2)', options);
+    %}
+    
+    clear galFea1 probFea1
+    trainTime = toc(t0);
+    
+    galFea2 = galFea(p(numClass/2+1 : end), : );
+    probFea2 = probeFea(p(numClass/2+1 : end), : );
+    
+    t0 = tic;
+%    dist = MahDist(M, galFea2 * W, probFea2 * W);
+    dist = D;
+    clear galFea2 probFea2 M W
+    matchTime = toc(t0);
+    
+    fprintf('Fold %d: ', nf);
+    fprintf('Training time: %.3g seconds. ', trainTime);    
+    fprintf('Matching time: %.3g seconds.\n', matchTime);
+    
+    cms(nf,:) = EvalCMC( -dist, 1 : numClass / 2, 1 : numClass / 2, numRanks );
+    clear dist
+    
+    fprintf(' Rank1,  Rank5, Rank10, Rank15, Rank20\n');
+    fprintf('%5.2f%%, %5.2f%%, %5.2f%%, %5.2f%%, %5.2f%%\n\n', cms(nf,[1,5,10,15,20]) * 100);
+end
+
+meanCms = mean(cms);
+plot(1 : numRanks, meanCms);
+title('CMC Curve');
+
+fprintf('The average performance:\n');
+fprintf(' Rank1,  Rank5, Rank10, Rank15, Rank20\n');
+fprintf('%5.2f%%, %5.2f%%, %5.2f%%, %5.2f%%, %5.2f%%\n\n', meanCms([1,5,10,15,20]) * 100);
