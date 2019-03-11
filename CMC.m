315ede39ef581f1575fd46f849d55b4cb8a266ce
Neelabhro Roy <neelabhro16171@iiitd.ac.in>
2019-03-11 21:10:44 +0530

    Add files via upload

---
+++ CMC.m
@@ -0,0 +1,21 @@
+function acc = CMC(dist,limit)
+
+[~,index] = sort(dist,2);
+
+rank = zeros(size(dist,1),1);
+
+for i = 1:size(index,1)
+    rank(i) = find(index(i,:) == i);
+end
+
+acc = zeros(1,limit);
+for i = 1:limit
+    acc(i) = 100*mean(rank<=i);
+end
+
+bar(1:limit, acc);
+xlabel('Rank')
+ylabel('Accuracy')
+title('CMC curve')
+
+end
