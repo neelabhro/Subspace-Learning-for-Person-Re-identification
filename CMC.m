4655716b2d7ad0f76a9c79996604041e085455ac
Neelabhro Roy <neelabhro16171@iiitd.ac.in>
2019-03-19 19:32:58 +0530

    Update CMC.m

--- CMC.m
+++ CMC.m
@@ -1,21 +1,20 @@
 function acc = CMC(dist,limit)
 
-[~,index] = sort(dist,2);
+    [~,index] = sort(dist,2);
+    rank = zeros(size(dist,1),1);
 
-rank = zeros(size(dist,1),1);
+    for i = 1:size(index,1)
+        rank(i) = find(index(i,:) == i);
+    end
 
-for i = 1:size(index,1)
-    rank(i) = find(index(i,:) == i);
-end
+    acc = zeros(1,limit);
+    for i = 1:limit
+        acc(i) = 100*mean(rank<=i);
+    end
 
-acc = zeros(1,limit);
-for i = 1:limit
-    acc(i) = 100*mean(rank<=i);
-end
-
-plot(1:limit, acc);
-xlabel('Rank')
-ylabel('Accuracy')
-title('CMC curve')
+    plot(1:limit, acc,'LineWidth',3);
+    xlabel('Rank')
+    ylabel('Accuracy')
+    title('CMC curve')
 
 end
