ca1bcb285a6d92ab56d09fddf9bc96e39a202e78
Neelabhro Roy <neelabhro16171@iiitd.ac.in>
2019-06-10 03:00:20 +0530

    Update CUHK01_LOMO_XQDA.m

--- CUHK01/CUHK01_LOMO_XQDA.m
+++ CUHK01/CUHK01_LOMO_XQDA.m
@@ -9,7 +9,7 @@ close all;
 feaFile = 'cuhk01_lomo.mat';
 %pcaFile = 'matlabPCA100.mat';
 
-numClass = 485;
+numClass = 970;
 numFolds = 10;
 numRanks = 100;
 
@@ -27,15 +27,17 @@ galFea2 = camA1(:, 486:970);
 probeFea1 = camB1(:, 1:485);
 probeFea2 = camB1(:, 486:970);
 
+    L = 0.000001;
     Lu = 0.05;
     Lv = 0.2;
     La = 0.2;
     Lp = 0.2;
+    Lw = 0.1;
 
-    nu = 1;
-    beta = 1;
+    nu = 0;
+    beta = 200;
 
-    n = 316;
+    n = 485;
     d = 100;
     k = d;
 
@@ -57,3 +59,112 @@ probeFea2 = camB1(:, 486:970);
     TestSet = zeros(970,35722);
     TestSet(1:485 ,:) = galFea2';
     TestSet(486: end,:) = probeFea2';
+    
+    
+%%    
+    
+    [X , W] = matlabPCA(TrainSet',100);
+    %load(pcaFile, 'X');
+    %load(pcaFile, 'W');
+    
+    X2 = X(:, 1:485);
+    X1 = X(:, 486:end);
+    %X1 = pca(probeFea1');
+    %X2 = pca(galFea1');
+    [W_XQDA, M] = XQDA(X1', X2', (1:numClass/2)', (1:numClass/2)');
+    
+    TestPCA = W' * TestSet';
+    X22 = TestPCA(:, 1:485);
+    X12 = TestPCA(:, 486:end);
+
+    
+    %X1 = 3.* randi([0, 1], [d,n]);
+    %X2 = X1 + 3;
+    %X1 = X1 - 3;
+
+
+    %X12 = 3.* randi([0, 1], [d,n]);
+    %X22 = X12 + 3;
+    %X12 = X12 - 3; 
+%     X1 = 3.* randi([0, 1], [d,n]);
+%     X2 = X1 + 0;
+%     X1 = X1 - 0;
+% 
+% 
+%     X12 = 3.* randi([0, 1], [d,n]);
+%     X22 = X12 + 0;
+%     X12 = X12 - 0;
+    %X12 = pca(probeFea2');
+    %X22 = pca(galFea2');
+
+    % n is the size of the sample set
+    % d is the feature dimension equal to 100
+    % Components of Probe set X1 and gallery set X2 are matched image descriptors
+    % K is the number of latent factors and is equal to d
+    % U is the basis matrix capturing latent intrinsic structure of the input
+    % data matrix
+    % V1 and V2 (kxn) indicate the semantic representation of X1 and X2
+
+    % X(dxn) = U(dxk)*V(kxn)
+    %X1 = randi([0, 1], [d,n]);
+    %X2 = randi([0, 1], [d,n]);
+
+    U  = randi([0, 1], [d,k]);
+    V1 = randi([0, 1], [k,n]);
+    V2 = randi([0, 1], [k,n]);
+    A  = randi([0, 1], [k,k]);
+    P1 = randi([0, 1], [k,d]);
+    P1 = eye(k);
+    P2 = randi([0, 1], [k,d]);
+    P2 = eye(k);
+    W2 = W_XQDA';
+
+
+
+%% Main algorithm
+    for i = 1:500
+        U  = (( W2*X1 * transpose(V1)) + ( W2*X2 * transpose(V2))) .* inv((( V1 * transpose(V1)) + ( V2 * transpose(V2)) + (Lu*eye(k))));
+        V1 = inv(((transpose(U) * U) + (nu + beta + Lv) * eye(k))) * ((transpose(U) *W2* X1) + (beta* A * V2) + nu * P1 * W2*X1);
+        V2 = inv(((transpose(U) * U) + ( beta * transpose(A) * A) + (nu + Lv) .* eye(k))) * ((transpose(U) *W2* X2) + (beta* transpose(A) * V1) + nu * P2*W2 * X2);
+        %P1 = (V1 * transpose(X1)) * inv((X1 * transpose(X1)) + (Lp/nu)*eye(k));
+        %P2 = (V2 * transpose(X2)) * inv((X2 * transpose(X2)) + (Lp/nu)*eye(k));
+        A  = (V1 * transpose(V2)) /((V2 * transpose(V2)) + (La/beta)*eye(k));
+        %W2 = (X1*V1'*U' + X2*V2'*U')*inv((X1*X1' + X2*X2' - Lw*eye(k)));
+        
+        %for j = 1:k
+        %    W2(:,j) = W2(:,j)./norm(W2(:,j));
+        %end
+        
+
+        %W2 = W1';
+        for j = 1:50
+            Vw = 2*W2'*X1*X1' - 2*(X1*V1')*U' + 2*W2'*X2*X2' - 2*(X2*V2')*U' - 2*nu*X1*V1'*P1' - 2*nu*X2*V2'*P2' + 2*nu*P1'*P1*W2'*X1*X1' + 2*nu*P2'*P2*W2'*X2*X2' + 2*Lw*W2';
+            W2 = W2 - L*Vw;
+            
+            %W=W./norm(W);
+        end 
+        %W=W+.01*eye(100);
+    end
+        V12 = inv(((transpose(U) * U) + (nu + beta + Lv) * eye(k))) * ((transpose(U) *W2* X12) + (beta* A * V2) + nu * P1 * W2*X12);
+        %for i = 1:50
+            
+            V22 = inv(((transpose(U) * U) + ( beta * transpose(A) * A) + (nu + Lv) .* eye(k))) * ((transpose(U) *W2* X22) + (beta* transpose(A) * V12) + nu * P2*W2 * X22);    
+            V12 = inv(((transpose(U) * U) + (nu + beta + Lv) * eye(k))) * ((transpose(U) *W2* X12) + (beta* A * V22) + nu * P1 * W2*X12);
+        %end    
+        V22 = inv(((transpose(U) * U) + ( beta * transpose(A) * A) + (nu + Lv) .* eye(k))) * ((transpose(U) *W2* X22) + (beta* transpose(A) * V12) + nu * P2*W2 * X22);    
+
+        D = zeros(n,n);
+    for m = 1:n
+    
+        %v1 = P1*(X12(:,m));
+        %v1 = (W2*X1(:,m));
+        v1 = V12(:,m);
+        for i = 1:n
+            %v2 = P2*(X22(:,i));
+            %v2 = (W2*X2(:,i));
+            v2 = V22(:,i);
+            D(m,i) = norm(((v1 -A* v2)));
+        end
+
+    end
+    CMC(D,100);
