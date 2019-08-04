4ba3de9c9eaffa4d95560f8e95400301f71ca088
Neelabhro Roy <neelabhro16171@iiitd.ac.in>
2019-08-04 15:19:21 +0530

    t-SNE

---
+++ Final_Approach/CSPLTest.m
@@ -0,0 +1,180 @@
+%CSPL Paper Replication 65% Accuracy
+%Neelabhro Roy
+%IIIT-Delhi
+
+clear;
+clc;
+close all;
+
+feaFile = 'viper_lomo.mat';
+pcaFile = 'matlabPCA100.mat';
+
+numClass = 632;
+numFolds = 10;
+numRanks = 100;
+
+%% load the extracted LOMO features
+load(feaFile, 'descriptors');
+galFea = descriptors(1 : numClass, :);
+probeFea = descriptors(numClass + 1 : end, :);
+clear descriptors
+
+    Lu = 0.05*1;
+    L = 0.000000001;
+    Lv = 0.2*1;
+    La = 0.2*1;
+    Lp = 0.2*1;
+    Lw = 0.5*1;
+
+    nu = 1*1;
+    beta = 1*1;
+
+    n = 316;
+    d = 100;
+    k = d;
+
+    p = randperm(numClass);
+    
+    galFea1 = galFea(p(1:numClass/2), : );
+    probeFea1 = probeFea(p(1:numClass/2), : );
+
+    TrainSet = zeros(632,26960);
+    TrainSet(1:316 ,:) = galFea1;
+    TrainSet(317: end,:) = probeFea1;
+    
+    t0 = tic;
+
+    trainTime = toc(t0);
+    galFea2 = galFea(p(numClass/2+1 : end), : );
+    probeFea2 = probeFea(p(numClass/2+1 : end), : );
+    
+    TestSet = zeros(632,26960);
+    TestSet(1:316 ,:) = galFea2;
+    TestSet(317: end,:) = probeFea2;
+    
+    %[X , W] = matlabPCA(TrainSet',100);
+    load(pcaFile, 'X');
+    load(pcaFile, 'W');
+    
+    X2 = X(:, 1:316);
+    X1 = X(:, 317:end);
+    %X1 = pca(probeFea1');
+    %X2 = pca(galFea1');
+    
+    TestPCA = W' * TestSet';
+    X22 = TestPCA(:, 1:316);
+    X12 = TestPCA(:, 317:end);
+
+    
+    %X1 = 3.* randi([0, 1], [d,n]);
+    %X2 = X1 + 3;
+    %X1 = X1 - 3;
+
+
+    %X12 = 3.* randi([0, 1], [d,n]);
+    %X22 = X12 + 3;
+    %X12 = X12 - 3; 
+%     X1 = 3.* randi([0, 1], [d,n]);
+%     X2 = X1 + 0;
+%     X1 = X1 - 0;
+% 
+% 
+%     X12 = 3.* randi([0, 1], [d,n]);
+%     X22 = X12 + 0;
+%     X12 = X12 - 0;
+    %X12 = pca(probeFea2');
+    %X22 = pca(galFea2');
+
+    % n is the size of the sample set
+    % d is the feature dimension equal to 100
+    % Components of Probe set X1 and gallery set X2 are matched image descriptors
+    % K is the number of latent factors and is equal to d
+    % U is the basis matrix capturing latent intrinsic structure of the input
+    % data matrix
+    % V1 and V2 (kxn) indicate the semantic representation of X1 and X2
+
+    % X(dxn) = U(dxk)*V(kxn)
+    %X1 = randi([0, 1], [d,n]);
+    %X2 = randi([0, 1], [d,n]);
+
+    U  = randi([0, 1], [d,k]);
+    V1 = randi([0, 1], [k,n]);
+    V2 = randi([0, 1], [k,n]);
+    A  = randi([0, 1], [k,k]);
+    P1 = randi([0, 1], [k,d]);
+    %P1 = eye(k);
+    P2 = randi([0, 1], [k,d]);
+    %P2 = eye(k);
+    W2 = eye(k);
+    
+
+
+
+%% Main algorithm
+    for i = 1:500
+        U  = (( W2*X1 * transpose(V1)) + ( W2*X2 * transpose(V2))) .* inv((( V1 * transpose(V1)) + ( V2 * transpose(V2)) + (Lu*eye(k))));
+        V1 = inv(((transpose(U) * U) + (nu + beta + Lv) * eye(k))) * ((transpose(U) *W2* X1) + (beta* A * V2) + nu * P1 * W2*X1);
+        V2 = inv(((transpose(U) * U) + ( beta * transpose(A) * A) + (nu + Lv) .* eye(k))) * ((transpose(U) *W2* X2) + (beta* transpose(A) * V1) + nu * P2*W2 * X2);
+        P1 = (V1 * transpose(X1)) * inv((X1 * transpose(X1)) + (Lp/nu)*eye(k));
+        P2 = (V2 * transpose(X2)) * inv((X2 * transpose(X2)) + (Lp/nu)*eye(k));
+        A  = (V1 * transpose(V2)) /((V2 * transpose(V2)) + (La/beta)*eye(k));
+        %W2 = inv((2*X1*X1' + 2*X2*X2' + Lw*eye(k)))*(X1*V1'*U' + X2*V2'*U' + X1*V1'*P1 + X2*V2'*P2);
+        %W2 = inv((4*X1*X1' + 4*X2*X2' + Lw*eye(k) -X1*X2' -X2*X1'))*(X1*V1'*U' + X2*V2'*U' + X1*V1'*P1 + X2*V2'*P2);
+        %W2 = W2';
+        %W2 = eye(k);
+        for j = 1:50
+            
+            Vw = 4*W2'*X1*X1' + 4*W2'*X2*X2' - 2*(X1*V1')*U'  - 2*(X2*V2')*U' - 2*nu*X1*V1'*P1' - 2*nu*X2*V2'*P2' + 2*nu*P1'*P1*W2'*X1*X1' + 2*nu*P2'*P2*W2'*X2*X2' + 2*Lw*W2' - X1*X2' - X2*X1';
+            W2 = W2 - L*Vw;
+            
+            %W=W./norm(W);
+        end 
+        W2 = W2';
+        
+    end
+
+    
+    D = zeros(n,n);
+    for m = 1:n
+    
+        v1 = P1*W2*(X12(:,m));
+    
+        for i = 1:n
+            v2 = P2*W2*(X22(:,i));
+            D(m,i) = norm(((v1 - A*v2)));
+        end
+        
+    end
+    
+CMC(D,100);
+figure;
+
+
+
+
+
+for i = 1:10
+    x = 5;  %Number of Identities to check
+    p1 = randperm(225);
+    
+    Y1 = tsne(X1(:,p1(1:x))');
+    Y2 = tsne((X2(:,p1(1:x)))');
+    Y3=[Y1;Y2];
+    G=[1:2];
+    scatter(Y3(1,1),Y3(1,2),50,G(1),'filled');
+    hold on;
+    scatter(Y3(6,1),Y3(6,2),50,G(2),'filled');
+    hold on;
+    
+    
+    Y4 = tsne(((W2')*(X1(:,p1(1:x))))');
+    Y5 = tsne(((W2')*(X2(:,p1(1:x))))');
+    Y6=[Y4;Y5];
+    G=[3:4];
+    scatter(Y6(1,1),Y6(1,2),50,G(1),'filled');
+    hold on;
+    scatter(Y6(6,1),Y6(6,2),50,G(2),'filled');
+    grid on;
+    legend('X1','X2','W_2 X1','W_2 X2');
+    figure;
+end    
